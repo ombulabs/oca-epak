9f3394351727c231df5c4c86cd4e24af5e35f6a0
Mauro Otonelli <mauro.otonelli@gmail.com>
2015-10-28 18:40:27 -0300

    Updated tests

--- spec/oca-epak_spec.rb
+++ spec/oca-epak_spec.rb
@@ -1,25 +1,26 @@
 RSpec.describe Oca do
+  let(:username) { "hey@you.com" }
+  let(:password) { "123456" }
+  subject { Oca.new(username, password) }
+
   context "authenticating a user" do
-    let(:username) { "hey@you.com" }
-    let(:password) { "123456" }
     let(:credentials) { { "usr" => username, "psw" => password } }
     let(:exception) { Savon::SOAPFault.new("", "") }
-    let(:oca) { Oca.new }
 
     it "returns true if the user has valid credentials" do
-      allow(oca.client).to(
+      allow(subject.client).to(
         receive(:call).with(:generar_consolidacion_de_ordenes_de_retiro,
           message: credentials)).and_raise(exception)
 
-      expect(oca.check_credentials(username, password)).to be_truthy
+      expect(subject.check_credentials).to be_truthy
     end
 
     it "returns false if the user has invalid credentials" do
-      allow(oca.client).to(
+      allow(subject.client).to(
         receive(:call).with(:generar_consolidacion_de_ordenes_de_retiro,
           message: credentials)).and_return({})
 
-      expect(oca.check_credentials(username, password)).to be_falsey
+      expect(subject.check_credentials).to be_falsey
     end
   end
 
@@ -27,18 +28,17 @@ RSpec.describe Oca do
     let(:cuit) { "30-99999999-7" }
     let(:operation_type) { "77790" }
     let(:exception) { NoMethodError }
-    let(:oca) { Oca.new }
 
     it "returns true if the operation type exists" do
       VCR.use_cassette("get_shipping_rates") do
-        expect(oca.check_operation(cuit, operation_type)).to be_truthy
+        expect(subject.check_operation(cuit, operation_type)).to be_truthy
       end
     end
 
     it "returns false if the operation type doesn't exist" do
-      allow(oca).to(receive(:get_shipping_rates)).and_raise(exception)
+      allow(subject).to(receive(:get_shipping_rates)).and_raise(exception)
 
-      expect(oca.check_operation(cuit, operation_type)).to be_falsey
+      expect(subject.check_operation(cuit, operation_type)).to be_falsey
     end
   end
 
@@ -50,7 +50,6 @@ RSpec.describe Oca do
     let(:destination_zip_code) { "2000" }
     let(:package_quantity) { "1" }
     let(:operation_type) { "77790" }
-    let(:oca) { Oca.new }
 
     it "returns the shipping price and estimated date" do
       opts = { wt: weight, vol: volume, origin: origin_zip_code,
@@ -58,7 +57,7 @@ RSpec.describe Oca do
         op: operation_type }
 
       VCR.use_cassette("get_shipping_rates") do
-        response = oca.get_shipping_rates(opts)
+        response = subject.get_shipping_rates(opts)
         expect(response).to be
         expect(response[:precio]).to eql("328.9000")
         expect(response[:ambito]).to eql("Regional")
