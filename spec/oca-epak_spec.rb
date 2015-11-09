ea5fa4939e217b6bee5229b101afd15435c4a74f
Mauro Otonelli <mauro.otonelli@gmail.com>
2015-11-09 14:49:11 -0300

    Updated lib and tests to use a new OCA WSDL.

--- spec/oca-epak_spec.rb
+++ spec/oca-epak_spec.rb
@@ -1,10 +1,12 @@
-RSpec.describe Oca do
+RSpec.describe Oca::Epak do
+  let(:cuit) { "30-99999999-7" }
   let(:username) { "hey@you.com" }
   let(:password) { "123456" }
   let(:invalid_password) { "654321" }
-  subject { Oca.new(username, password) }
 
-  context "authenticating a user" do
+  subject { Oca::Epak.new(username, password) }
+
+  describe "#check_credentials" do
     it "returns true if the user has valid credentials" do
       VCR.use_cassette("check_credentials_are_valid") do
         expect(subject.check_credentials).to be_truthy
@@ -20,10 +22,8 @@ RSpec.describe Oca do
     end
   end
 
-  context "checking an operation type exists" do
-    let(:cuit) { "30-99999999-7" }
+  describe "#check_operation" do
     let(:operation_type) { "77790" }
-    let(:exception) { NoMethodError }
 
     it "returns true if the operation type exists" do
       VCR.use_cassette("get_shipping_rates") do
@@ -32,14 +32,44 @@ RSpec.describe Oca do
     end
 
     it "returns false if the operation type doesn't exist" do
-      allow(subject).to(receive(:get_shipping_rates)).and_raise(exception)
-
-      expect(subject.check_operation(cuit, operation_type)).to be_falsey
+      VCR.use_cassette("get_shipping_rates_invalid") do
+        expect(subject.check_operation(cuit, operation_type)).to be_falsey
+      end
     end
   end
 
-  context "getting shipping rates" do
-    let(:cuit) { "30-99999999-7" }
+  describe "#get_operation_codes" do
+    context "valid user + password" do
+      let(:expected_result) do
+        { :id_operativa=>"259563",
+          :descripcion=>"259563 - ENVIOS DE SUCURSAL A SUCURSAL",
+          :con_volumen=>false,
+          :con_valor_declarado=>false,
+          :a_sucursal=>false,
+          :"@diffgr:id"=>"Table1",
+          :"@msdata:row_order"=>"0" }
+      end
+
+      it "returns all the operations available for the user" do
+        VCR.use_cassette("get_operation_codes") do
+          result = subject.get_operation_codes
+          expect(result).to eql(expected_result)
+        end
+      end
+    end
+
+    context "invalid user" do
+      it "raises when they attempt to get operation codes" do
+        VCR.use_cassette("get_operation_codes_bad_request") do
+          expect do
+            subject.get_operation_codes
+          end.to raise_exception(Oca::Epak::BadRequest)
+        end
+      end
+    end
+  end
+
+  describe "#get_shipping_rates" do
     let(:weight) { "50" }
     let(:volume) { "0.027" }
     let(:origin_zip_code) { "1646" }
@@ -47,7 +77,7 @@ RSpec.describe Oca do
     let(:package_quantity) { "1" }
     let(:operation_type) { "77790" }
 
-    it "returns the shipping price and estimated date" do
+    it "returns the shipping price and estimated days until delivery" do
       opts = { wt: weight, vol: volume, origin: origin_zip_code,
         destination: destination_zip_code, qty: package_quantity, cuit: cuit,
         op: operation_type }
@@ -55,9 +85,9 @@ RSpec.describe Oca do
       VCR.use_cassette("get_shipping_rates") do
         response = subject.get_shipping_rates(opts)
         expect(response).to be
-        expect(response[:precio]).to eql("328.9000")
-        expect(response[:ambito]).to eql("Regional")
-        expect(response[:plazo_entrega]).to eql("3")
+        expect(response[:precio]).to eql("396.6900")
+        expect(response[:ambito]).to eql("Nacional 1")
+        expect(response[:plazo_entrega]).to eql("9")
       end
     end
   end
